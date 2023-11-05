109c586279881e3becbbc9f48d95d0d0b29b6dc3
jsuh <jsuh@hexacta.com>
2023-11-05 01:04:49 -0300

    feat: add s3 port config & reorder properties

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,9 +1,11 @@
-const JWT_SECRET = process.env.JWT_SECRET;
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+const JWT_SECRET = process.env.JWT_SECRET;
+const S3_PORT = process.env.S3_PORT;
 
 module.exports = Object.freeze({
-  JWT_SECRET,
   AWS_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY,
+  JWT_SECRET,
+  S3_PORT,
 });
