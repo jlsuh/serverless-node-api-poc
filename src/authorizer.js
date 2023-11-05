98be07154b4b5b2124b193b7f1f448ad78bcf88a
jsuh <jsuh@hexacta.com>
2023-11-05 11:09:26 -0300

    refactor: authorizer allow & deny policy

--- src/authorizer.js
+++ src/authorizer.js
@@ -1,17 +1,15 @@
 const config = require("./constants/appConstants");
 const jwt = require("jsonwebtoken");
 
-module.exports.handler = (event, _, callback) => {
+module.exports.handler = async (event) => {
   const token = event.authorizationToken.replace(/Bearer /g, "");
   const secret = config.JWT_SECRET;
-  jwt.verify(token, secret, (err) => {
-    if (err) {
-      callback(null, denyPolicy("anonymous", event.methodArn));
-    } else {
-      const { username } = jwt.decode(token);
-      callback(null, allowPolicy(username, event.methodArn));
-    }
-  });
+  try {
+    const { username } = jwt.verify(token, secret);
+    return allowPolicy(username, event.methodArn);
+  } catch (err) {
+    return denyPolicy("anonymous", event.methodArn);
+  }
 };
 
 const generatePolicy = (principalId, effect, resource) => {
