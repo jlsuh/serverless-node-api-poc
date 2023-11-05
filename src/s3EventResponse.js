47f949446dab8fe7aba46d6ea67ba505510dacaa
jsuh <jsuh@hexacta.com>
2023-11-05 09:44:10 -0300

    chore: change callback message

--- src/s3EventResponse.js
+++ src/s3EventResponse.js
@@ -2,5 +2,5 @@ module.exports.handler = (event, context, callback) => {
   console.info("event:", JSON.stringify(event, null, 2));
   console.info("context:", JSON.stringify(context, null, 2));
   console.info("process.env:", JSON.stringify(process.env, null, 2));
-  callback(null, "ok");
+  callback(null, "S3 Event Response");
 };
