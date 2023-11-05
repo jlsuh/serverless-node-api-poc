671e3a8386d869c8e3c5a4586b964d418beb596a
jsuh <jsuh@hexacta.com>
2023-11-05 00:43:21 -0300

    feat: add s3 configs

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,5 +1,9 @@
 const JWT_SECRET = process.env.JWT_SECRET;
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 
 module.exports = Object.freeze({
   JWT_SECRET,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
 });
