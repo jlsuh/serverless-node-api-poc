299a45ddc288090ab479ca32739e3aad91c3fe18
jsuh <jsuh@hexacta.com>
2023-11-05 01:05:51 -0300

    refactor: use s3 port config

--- src/s3Client.js
+++ src/s3Client.js
@@ -7,5 +7,5 @@ module.exports.s3Client = new S3Client({
     accessKeyId: config.AWS_ACCESS_KEY_ID,
     secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
   },
-  endpoint: "http://localhost:9091",
+  endpoint: `http://localhost:${config.S3_PORT}`,
 });
