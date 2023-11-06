64e17d7b4a50ec7e2c7e47112c1a0ddd02f1688f
jsuh <jsuh@hexacta.com>
2023-11-06 11:09:06 -0300

    feat: s3 list available keys from bucket

---
+++ src/s3GetBucket.js
@@ -0,0 +1,38 @@
+const { s3Client } = require("./s3Client");
+const { ListObjectsV2Command, NoSuchBucket } = require("@aws-sdk/client-s3");
+
+module.exports.handler = async (event) => {
+  const { bucketName } = event.pathParameters;
+  try {
+    const listObjectsV2CommandOutput = await s3Client.send(
+      new ListObjectsV2Command({
+        Bucket: bucketName,
+      }),
+    );
+    const { KeyCount, Contents } = listObjectsV2CommandOutput;
+    return !!KeyCount
+      ? {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: `Bucket '${bucketName}' has ${KeyCount} object(s)`,
+            keys: Contents.map((content) => `${content.Key}`),
+          }),
+        }
+      : {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: `Bucket '${bucketName}' is empty`,
+          }),
+        };
+  } catch (error) {
+    if (error instanceof NoSuchBucket) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: `Bucket '${bucketName}' not found`,
+        }),
+      };
+    }
+    throw new Error("Unidentified error");
+  }
+};
