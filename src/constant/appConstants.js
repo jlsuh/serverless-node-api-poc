367765aefa9721cab1943b4c872ddd542f08db7e
jsuh <jsuh@hexacta.com>
2023-11-08 17:18:11 -0300

    feat: connect to postgres

--- src/constant/appConstants.js
+++ src/constant/appConstants.js
@@ -5,6 +5,11 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_TOKEN_EXPIRATION = process.env.JWT_TOKEN_EXPIRATION;
 const MIN_USERNAME_LENGTH = +process.env.MIN_USERNAME_LENGTH;
 const POLICY_LANGUAGE_VERSION = process.env.POLICY_LANGUAGE_VERSION;
+const POSTGRES_DB = process.env.POSTGRES_DB;
+const POSTGRES_HOST = process.env.POSTGRES_HOST;
+const POSTGRES_HOST_PORT = process.env.POSTGRES_HOST_PORT;
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+const POSTGRES_USER = process.env.POSTGRES_USER;
 const S3_PORT = process.env.S3_PORT;
 
 module.exports = Object.freeze({
@@ -15,5 +20,10 @@ module.exports = Object.freeze({
   JWT_TOKEN_EXPIRATION,
   MIN_USERNAME_LENGTH,
   POLICY_LANGUAGE_VERSION,
+  POSTGRES_DB,
+  POSTGRES_HOST_PORT,
+  POSTGRES_HOST,
+  POSTGRES_PASSWORD,
+  POSTGRES_USER,
   S3_PORT,
 });
---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+const { Pool } = require("pg");
+const config = require("../constant/appConstants");
+
+const pool = new Pool({
+  database: config.POSTGRES_DB,
+  host: config.POSTGRES_HOST,
+  password: config.POSTGRES_PASSWORD,
+  port: config.POSTGRES_HOST_PORT,
+  user: config.POSTGRES_USER,
+});
+
+module.exports.query = async (text, params, callback) => {
+  return pool.query(text, params, callback);
+};
