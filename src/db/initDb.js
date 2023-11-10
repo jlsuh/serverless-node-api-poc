b93ab449f55fe8635e263fa92c6c8946b569386f
jsuh <jsuh@hexacta.com>
2023-11-10 19:19:52 -0300

    refactor: extract function

--- src/db/initDb.js
+++ src/db/initDb.js
@@ -1,26 +1,15 @@
 const { getClient } = require("./index");
 const users = require("../data/users");
 
+const SCHEMA = "serverless_node_api";
+
 module.exports.handler = async () => {
   const client = await getClient();
   try {
     await client.query("BEGIN");
-    await client.query("DROP SCHEMA IF EXISTS serverless_node_api CASCADE");
-    await client.query("CREATE SCHEMA serverless_node_api");
-    await client.query(
-      `CREATE TABLE serverless_node_api.user
-      (
-          id       SERIAL PRIMARY KEY,
-          username VARCHAR(100) NOT NULL,
-          fullname VARCHAR(100) NOT NULL
-      )`,
-    );
-    await client.query(
-      `INSERT INTO serverless_node_api.user (username, fullname)
-      SELECT x.username, x.fullname
-      FROM json_to_recordset($1) AS x(username VARCHAR(100), fullname VARCHAR(100))`,
-      [JSON.stringify(users)],
-    );
+    await client.query(`DROP SCHEMA IF EXISTS ${SCHEMA} CASCADE`);
+    await client.query(`CREATE SCHEMA ${SCHEMA}`);
+    await createUsers(client);
     await client.query("COMMIT");
   } catch (error) {
     await client.query("ROLLBACK");
@@ -28,4 +17,22 @@ module.exports.handler = async () => {
   } finally {
     client.release();
   }
+};
+
+const createUsers = async (client) => {
+  const TABLE = "user";
+  await client.query(
+    `CREATE TABLE ${SCHEMA}.${TABLE}
+      (
+          id       SERIAL PRIMARY KEY,
+          username VARCHAR(100) NOT NULL,
+          fullname VARCHAR(100) NOT NULL
+      )`,
+  );
+  await client.query(
+    `INSERT INTO ${SCHEMA}.${TABLE} (username, fullname)
+      SELECT x.username, x.fullname
+      FROM json_to_recordset($1) AS x(username VARCHAR(100), fullname VARCHAR(100))`,
+    [JSON.stringify(users)],
+  );
 };
