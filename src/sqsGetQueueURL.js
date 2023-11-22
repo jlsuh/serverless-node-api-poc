a6403dc0d016bbe7f1052e94077a6394c83f0769
jsuh <jsuh@hexacta.com>
2023-11-22 16:38:02 -0300

    refactor: get client url

--- src/sqsGetQueueURL.js
+++ src/sqsGetQueueURL.js
@@ -1,7 +1,5 @@
 import { GetQueueUrlCommand } from "@aws-sdk/client-sqs";
 import sqsClient from "./sqsClient.js";
 
-export async function getQueueUrl(queueName) {
-  const command = new GetQueueUrlCommand({ QueueName: queueName });
-  return await sqsClient.send(command);
-}
+export const getQueueUrl = async (queueName) =>
+  await sqsClient.send(new GetQueueUrlCommand({ QueueName: queueName }));
