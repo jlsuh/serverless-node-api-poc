782056372d8aad55fe22a21dc0e5030e35a36456
jsuh <jsuh@hexacta.com>
2023-11-28 10:42:48 -0300

    refactor: extract write audit log & status as failed on dead letter

--- src/auditConsumer.js
+++ src/auditConsumer.js
@@ -1,46 +1,16 @@
-import { config } from "./constant/appConstants.js";
-import { getObject } from "./getObject.js";
-import { putObject } from "./putObject.js";
+import { writeAuditLog } from "./writeAuditLog.js";
 
-const getTimeStamp = (sentTimestamp) =>
-  `[${new Date(+sentTimestamp).toLocaleString().split(", ").join(" - ")}]`;
-
-const composeNewData = (event) =>
-  event.Records.reduce((data, record) => {
-    const { body, attributes } = record;
-    const { SentTimestamp } = attributes;
-    const timeStamp = getTimeStamp(SentTimestamp);
-    return `${data}${timeStamp}: ${body}\n`;
-  }, "");
-
-const getExistingData = async () => {
-  try {
-    const object = await getObject({
-      bucketName: config.S3_LOCAL_BUCKET_NAME,
-      objectName: config.S3_AUDIT_OBJECT_KEY,
-    });
-    return JSON.parse(await object.Body.transformToString());
-  } catch (error) {
-    return "";
-  }
-};
-
-const simulateDLQ = () => {
+/*
+const simulateDLQTrigger = () => {
   const random = Math.random();
   console.log(`>>>>>>>>>> [DLQ %] <${random}> <<<<<<<<<<`);
-  if (random > 0.5) {
+  if (random > 0.8) {
     throw new Error("DLQ triggered.");
   }
 };
+*/
 
 export async function handler(event) {
-  simulateDLQ();
-  const existingData = await getExistingData();
-  const newData = composeNewData(event);
-  const data = existingData + newData;
-  await putObject({
-    bucketName: config.S3_LOCAL_BUCKET_NAME,
-    data,
-    objectKey: config.S3_AUDIT_OBJECT_KEY,
-  });
+  // simulateDLQTrigger();
+  writeAuditLog(event);
 }
--- src/auditDeadLetterConsumer.js
+++ src/auditDeadLetterConsumer.js
@@ -1,4 +1,16 @@
+import { statuses } from "./constant/appConstants.js";
+import { writeAuditLog } from "./writeAuditLog.js";
+
 export async function handler(event) {
-  console.log(">>>>>>>>> From auditDeadLetterConsumer <<<<<<<<<");
-  console.log("event: ", event);
+  const failedEvent = {
+    ...event,
+    Records: event.Records.map((record) => ({
+      ...record,
+      body: JSON.stringify({
+        ...JSON.parse(record.body),
+        status: statuses.FAILURE,
+      }),
+    })),
+  };
+  writeAuditLog(failedEvent);
 }
